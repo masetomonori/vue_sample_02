bf8d63e6bdcaa349aed85bc5b9bd942fbde3ed9a
tomtom <tomtom@masetomookennoMacBook-Pro.local>
2022-06-15 15:39:48 +0900

    create view

--- src/components/TaskCreateComponent.vue
+++ src/components/TaskCreateComponent.vue
@@ -1,26 +1,23 @@
 <template>
-    <div>
-        <div>
-            <div>
-                <form v-on:submit.prevent="submit">
-                    <div>
-                        <label>Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.name">
-                    </div>
-                    <div>
-                        <label>Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.course">
-                    </div>
-                    <!--div>
-                        <label>Person In Charge</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-charge">
-                    </div-->
-                    <button type="submit" class="btn btn-primaryy">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
+    <v-form>
+        <v-container>
+            <v-row>
+            <v-col cols="10">
+            <v-text-field
+                v-model="task.name"
+                label="名前"
+            ></v-text-field>
+            <v-text-field
+                v-model="task.course"
+                label="所属"
+            ></v-text-field>
+            <v-btn  @click="submit">Submit</v-btn>
+            </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
+
 
 <script>
     import axios from 'axios'
--- src/components/TaskEditComponent.vue
+++ src/components/TaskEditComponent.vue
@@ -1,31 +1,3 @@
-<!--template>
-    <div>
-        <div>
-            <div>
-                <form v-on:submit.prevent="submit">
-                    <div>
-                        <label>ID</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task.id">
-                    </div>
-                    <div>
-                        <label>Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.name">
-                    </div>
-                    <div>
-                        <label>Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.course">
-                    </div>
-                    <div>
-                        <label>Person In Charge</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-charge">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</template-->
-
 <template>
     <v-form>
         <v-container>
